883fde91e938038d7e26aeb61c4d9275f7111c14
Kot E <ekattow9@gmail.com>
2022-08-15 18:41:43 +0300

    fixes

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        price = 10_000;
         int bonus = 20;
         int miles = price / bonus;
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
         public static void main(String[] args) {
             BonusMilesService service = new BonusMilesService();
-            int price = 10_000;
+            int price = 8_111;
             int miles = service.calculate(price);
             System.out.println(miles);
         }
